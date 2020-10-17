3b6ef97a863a6fabd87854f5c4677fb55cd60c72
Nic Mayer <42087659+nicm42@users.noreply.github.com>
2020-10-17 14:05:50 +0100

    Updated the random function, making the fireworks look more firework-like

--- script.js
+++ script.js
@@ -51,7 +51,5 @@ function fireworks() {
 }
 
 function random(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return min + Math.random() * (max + 1 - min);
 }

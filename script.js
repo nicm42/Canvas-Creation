5580fce3bafd8a66b1521c9751ac528a3beae291
Nic Mayer <42087659+nicm42@users.noreply.github.com>
2020-10-19 06:43:25 +0100

    Updated it to just be stars and fireworks

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ window.addEventListener('resize', () => {
 });
 
 //Fireworks canvas
-const canvas3 = document.querySelector('#canvas3');
-const ctx = canvas3.getContext("2d");
+const canvas2 = document.querySelector('#canvas2');
+const ctx = canvas2.getContext("2d");
 
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
@@ -23,58 +23,21 @@ window.addEventListener('resize', () => {
   ctx.canvas.height = window.innerHeight;
 });
 
-//Fire canvas
-const canvas2 = document.querySelector('#canvas2');
-const ctxFire = canvas2.getContext("2d");
-
-ctxFire.canvas.width = window.innerWidth;
-ctxFire.canvas.height = window.innerHeight;
-window.addEventListener('resize', () => {
-  ctxFire.canvas.width = window.innerWidth;
-  ctxFire.canvas.height = window.innerHeight;
-  //Re-draw the fire
-  ctxFire.clearRect(0, 0, canvas2.width, canvas2.height);  
-  fire();
-});
-
-//Set up variables
-let skyStart = window.innerHeight * 3 / 4;
-let fireEnd = window.innerWidth / 10;
-let fireworkStart = window.innerHeight / 2;
-
-window.addEventListener('resize', () => {
-  skyStart = window.innerHeight * 3 / 4;
-  fireEnd = window.innerWidth / 6;
-  fireworkStart = window.innerHeight / 2;
-});
-
 let randomFireworks = getRandomIntInclusive(5,10);
-let numberCatherineWheels = getRandomIntInclusive(3,5);
-
-let fireworkCount = 0;
-let catherineWheelCount = 0;
 
 //Add all the elements
-ground();
 stars();
-fire();
-//fireworks();
-
-function ground(){
-  ctxBkg.fillStyle = '#181818';
-  ctxBkg.globalAlpha = 1;
-  ctxBkg.fillRect(0, skyStart, ctxBkg.canvas.width, skyStart);
-}
+fireworks();
 
 function stars() {
   //Add stars to a small fraction of the canvas
-  const canvasSize = ctxBkg.canvas.width * skyStart;
+  const canvasSize = ctxBkg.canvas.width * ctxBkg.canvas.height;
   const stars = canvasSize / 10000;
 
   for(let i = 0; i < stars; i++) {
     //Set up random elements
     let xPos = random(2, ctxBkg.canvas.width - 2);
-    let yPos = random(2, skyStart - 2);
+    let yPos = random(2, ctxBkg.canvas.height - 2);
     let alpha = random(.5, 1);
     let size = random(1, 2);
 
@@ -85,77 +48,10 @@ function stars() {
   }
 }
 
-function fire() {
-  ctxFire.globalAlpha = 1;
-  let firePixelWidth = 4;
-  let leftStart = 0;
-  let rowCount = 0;
-  let greenCount = 15;
-
-  let rowRed = 'ff';
-  let rowGreen1 = 'f';
-  let rowGreen2 = 'f';
-  let rowBlue = 'ff';
-
-  for (let row = 15; row >= 0; row--) {
-    //Going from white (#fffff) to orange (#ff7000)
-    if(row % 2 !== 0) {
-      greenCount --;
-      rowGreen1 = greenCount.toString(16);
-    }
-    rowGreen2 = row.toString(16);
-    let rowGreen = rowGreen1 + rowGreen2;
-    rowBlue = row.toString(16) + row.toString(16);
-    let rowColour = '#' + rowRed + rowGreen + rowBlue;
-    for (let col = 0; col < fireEnd / firePixelWidth; col++) {
-      leftStart = col * firePixelWidth;
-      bottomStart = ctxFire.canvas.height - (rowCount * 5);
-      createBase(rowColour);
-    }
-    rowCount++;
-    fireEnd--;
-  }
-
-  createFlames();
-
-  function createBase(rowColour) {
-    ctxFire.beginPath();
-    ctxFire.arc(leftStart, bottomStart, firePixelWidth, 0, 2 * Math.PI);
-    ctxFire.fillStyle = rowColour;
-    ctxFire.fill();
-  }
-
-  function createFlames() {
-    const numberOfFlames = fireEnd / firePixelWidth / 4;
-    let flameLeft = 5;
-    for (let flame = 0; flame < numberOfFlames; flame++) {
-      let flameBottom = bottomStart - 2;
-      flameLeft += 4;
-      greenCount = 7;
-      rowGreen1 = '7';
-      for (let index = 15; index >= 0; index--) {
-        rowRed = index.toString(16) + index.toString(16);
-        //Going from orange (#ff7000) to grey (#000000)
-        if(index % 2 === 0 && index > 0) {
-          greenCount --;
-          rowGreen1 = greenCount.toString(16);
-        }
-        let rowFlameColour = '#' + rowRed + rowGreen1 + rowGreen2 + rowBlue;
-        ctxFire.beginPath();
-        ctxFire.arc(flameLeft, flameBottom, firePixelWidth, 0, 2 * Math.PI);
-        ctxFire.fillStyle = rowFlameColour; 
-        ctxFire.fill();
-        flameBottom -= 4;
-        flameLeft += 0.5;
-      }
-    }
-  }
-}
-
 function fireworks() {
   let particles = [];
-  const startX = random(10, canvas1.width - 10);
-  const startY = random(10, fireworkStart - 100);
+  const startX = random(10, canvas2.width - 10);
+  const startY = random(10, canvas2.height - 100);
   const colour = '#' + Math.random().toString(16).substr(2, 6);
   let alpha = random(0.5, 1);
   drawFireworks();
@@ -183,75 +79,11 @@ function fireworks() {
       window.requestAnimationFrame(drawFireworks);
     } else {
       window.cancelAnimationFrame(drawFireworks);
-      ctx.clearRect(0, 0, canvas3.width, canvas3.height);
-      fireworkCount++;
-      if(fireworkCount === randomFireworks) {
-        numberCatherineWheels = getRandomIntInclusive(3,5);
-        catherineWheel(numberCatherineWheels);
-        fireworkCount = 0;
-      } else {
-        fireworks();
-      }
+      ctx.clearRect(0, 0, canvas2.width, canvas2.height);
+      fireworks();
     }
   
   }
-}
-
-function catherineWheel(numberCatherineWheels) {
-  const colour = '#' + Math.random().toString(16).substr(2, 6);
-  let alpha = random(0.5, 1);
-  let canvasX = random(50, canvas3.width - 50);
-  ctx.lineWidth = 1;
-  ctx.strokeStyle = colour;
-  const randomDirection = getRandomIntInclusive(1,2); //1 = anticlockwise, 2 = clockwise 
-  let direction = true;
-  if(randomDirection === 2){
-    direction = false;
-  }
-  const startPoint = random(0,6.3);
-  let endPoint = 6.2;
-  if(!direction){
-    endPoint = 0.1;
-  }
-
-  drawCatherineWheel();
-
-  function drawCatherineWheel() {
-    ctx.beginPath();
-    ctx.arc(canvasX, skyStart, 50, startPoint, endPoint, direction);
-    ctx.globalAlpha = alpha;
-    ctx.stroke();
-    if(direction) {
-      endPoint -= 0.1
-    } else {
-      endPoint += 0.1;
-    }
-    alpha -= 0.01;
-    if((direction && endPoint > 0) || (!direction && endPoint < 6.4)){
-      requestAnimationFrame(drawCatherineWheel);
-    } else {
-      window.cancelAnimationFrame(drawCatherineWheel);
-      let startOpacity = 1;
-      const fading = setInterval(() => {
-        canvas3.style.opacity = startOpacity - 0.1;
-        startOpacity -= 0.1;
-      }, 100);
-      setTimeout(() => {
-        clearInterval(fading);
-        ctx.clearRect(0, 0, canvas3.width, canvas3.height);        
-        canvas3.style.opacity = 1;
-        catherineWheelCount++;
-        if(catherineWheelCount < numberCatherineWheels) {
-          catherineWheel(numberCatherineWheels);
-        }else {
-          catherineWheelCount = 0;
-          randomFireworks = getRandomIntInclusive(5,10);
-          fireworks();
-        }
-      }, 1000);
-    }
-  }
-
 }
 
 function random(min, max) {
